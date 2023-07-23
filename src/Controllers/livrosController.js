33dc2299250799cec3d6d6bd225bb82135a7a402
CaioSC01 <caio180g@gmail.com>
2023-07-23 13:22:43 -0300

    Aplicando Post no controller

--- src/Controllers/livrosController.js
+++ src/Controllers/livrosController.js
@@ -6,6 +6,20 @@ class LivroController {
       res.status(200).json(livros);
     });
   };
+
+  static criarLivros = (req, res) => {
+    let livro = new livros(req.body);
+
+    livro.save((err) => {
+      if (err) {
+        req
+          .status(500)
+          .send({ message: `${err.message}- falaha ao cadastrar livro.` });
+      } else {
+        res.status(201).send(livro.toJson());
+      }
+    });
+  };
 }
 
 export default LivroController;
