e47cb4fc53657a081c4113e031d39d247f478f16
CaioSC01 <caio180g@gmail.com>
2023-07-23 12:55:21 -0300

    Fazendo get no router

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -2,3 +2,7 @@ import express from "express";
 import LivroController from "../Controllers/livrosController";
 
 const router = express.Router();
+
+router.get("/livros", LivroController.listarLivros);
+
+export default router;
