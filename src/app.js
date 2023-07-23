b40b46998959bb55f425d9f3366e48fe471aad0a
CaioSC01 <caio180g@gmail.com>
2023-07-23 14:24:04 -0300

    Deixando codigo mais limpo

--- src/app.js
+++ src/app.js
@@ -10,39 +10,8 @@ db.once("open", () => {
 
 const app = express();
 
-routes(app)
+routes(app);
 
 app.use(express.json());
-
-app.get("/livros/:id", (req, res) => {
-  let index = buscaLivro(req.params.id);
-  res.json(livros[index]);
-});
-
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
-});
-
-app.post("/livros", (req, res) => {
-  livros.push(req.body);
-  res.status(201).send("Livro foi cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-  let index = buscaLivro(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.json(livros);
-});
-
-app.delete("/livros/:id", (req, res) => {
-  let { id } = req.params;
-  let index = buscaLivro(id);
-  livros.splice(index, 1);
-  res.send(`Livro ${id} removido com sucesso`);
-});
-
-function buscaLivro(id) {
-  return livros.findIndex((livro) => livro.id == id);
-}
 
 export default app;
