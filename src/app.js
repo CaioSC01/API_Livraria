4da5d557bf03d30ee6c3b1af8ea7401e1ea710df
CaioSC01 <caio180g@gmail.com>
2023-07-25 10:51:12 -0300

    Extruturando uma middleway de erros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./Config/dbConnect.js";
 import routes from "./routes/index.js";
+import mongoose from "mongoose";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -8,9 +9,18 @@ db.once("open", () => {
 });
 
 const app = express();
-
+app.use(express.json());
 routes(app);
 
-app.use(express.json());
+// eslint-disable-next-line no-unused-vars
+app.use((erro, req, res, next) => {
+    if (erro instanceof mongoose.Error.CastError) {
+        res.status(400).send({
+            message: "Um ou mais dados fornecidos estão incorretos",
+        });
+    } else {
+        res.status(500).json({ message: "Erro interno no servidor" });
+    }
+});
 
 export default app;
