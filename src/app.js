493602501241836ce0a0788c2759f0ef96660272
CaioSC01 <caio180g@gmail.com>
2023-07-25 11:02:03 -0300

    Limpando codigos e criando arquivo para a middle

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import db from "./Config/dbConnect.js";
 import routes from "./routes/index.js";
-import mongoose from "mongoose";
+import manipuladorDeErros from "./Middlewares/manipuladorDeErros.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -13,14 +13,6 @@ app.use(express.json());
 routes(app);
 
 // eslint-disable-next-line no-unused-vars
-app.use((erro, req, res, next) => {
-    if (erro instanceof mongoose.Error.CastError) {
-        res.status(400).send({
-            message: "Um ou mais dados fornecidos estão incorretos",
-        });
-    } else {
-        res.status(500).json({ message: "Erro interno no servidor" });
-    }
-});
+app.use(manipuladorDeErros);
 
 export default app;
