bbbd2d748be97794d4ff0d2433baf163bb683a44
CaioSC01 <caio180g@gmail.com>
2023-07-27 14:10:44 -0300

    Adicionando erro 404 e manipulando ele

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./Config/dbConnect.js";
 import routes from "./routes/index.js";
 import manipuladorDeErros from "./Middlewares/manipuladorDeErros.js";
+import manipulador404 from "./Middlewares/manipulador404.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 routes(app);
 
+app.use(manipulador404);
 // eslint-disable-next-line no-unused-vars
 app.use(manipuladorDeErros);
 
