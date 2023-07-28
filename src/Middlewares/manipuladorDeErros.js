7638a7e6e4c70eb0ce4facdc516da7a7e638801a
CaioSC01 <caio180g@gmail.com>
2023-07-28 14:34:49 -0300

    Adicionando paginar

--- src/Middlewares/manipuladorDeErros.js
+++ src/Middlewares/manipuladorDeErros.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import ErroBase from "../Erros/ErroBase.js";
-import NaoEncontrado from "../Erros/NaoEncontrado.js";
-import ErroValidacao from "../Erros/ErroValidacao.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 import RequisicaoIncorreta from "../Erros/RequisicaoIncorreta.js";
 
 // eslint-disable-next-line no-unused-vars
@@ -10,11 +9,10 @@ function manipuladorDeErros(erro, req, res, next) {
         new RequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(erro).enviarResposta(res);
-    } else if (erro instanceof NaoEncontrado) {
+    } else if (erro instanceof ErroBase) {
         erro.enviarResposta(res);
     } else {
         new ErroBase().enviarResposta(res);
     }
 }
-
 export default manipuladorDeErros;
