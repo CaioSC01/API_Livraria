42cae1d75501d019fe2085de7d590ac62ab3d0fc
CaioSC01 <caio180g@gmail.com>
2023-07-28 14:35:21 -0300

    ...

--- src/Controllers/livrosController.js
+++ src/Controllers/livrosController.js
@@ -1,15 +1,15 @@
-import NaoEncontrado from "../erros/NaoEncontrado.js";
+import NaoEncontrado from "../Erros/NaoEncontrado.js";
+// import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../Models/index.js";
 
 class LivroController {
     static listarLivros = async (req, res, next) => {
         try {
-            const livrosResultado = await livros
-                .find()
-                .populate("autor")
-                .exec();
+            const buscaLivros = livros.find();
 
-            res.status(200).json(livrosResultado);
+            req.resultado = buscaLivros;
+
+            next();
         } catch (erro) {
             next(erro);
         }
@@ -87,11 +87,11 @@ class LivroController {
             const busca = await processaBusca(req.query);
 
             if (busca !== null) {
-                const livrosResultado = await livros
-                    .find(busca)
-                    .populate("autor");
+                const livrosResultado = livros.find(busca).populate("autor");
 
-                res.status(200).send(livrosResultado);
+                req.resultado = livrosResultado;
+
+                next();
             } else {
                 res.status(200).send([]);
             }
